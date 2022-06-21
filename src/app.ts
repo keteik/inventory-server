680981b1038d93e3b372f8be0809348f55e90e82
keteik <ki3t3k@gmail.com>
2022-06-21 22:02:13 +0200

    initial commit

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,8 @@
-import { Request, Response } from "express";
-
 import mongoose from "mongoose"; 
 import express from 'express';
 import 'dotenv/config';
 
 const app = express();
-
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
@@ -20,17 +17,17 @@ async function startServer(): Promise<void> {
     const HTTP_PORT: string | undefined = process.env.HTTP_PORT;
 
     if(!MONGO_URI)
-        throw new Error("URI for connection to database not specified!");
+        throw new Error("URI for DB is not specified or invalid!");
         
     if(!HTTP_PORT)
-        throw new Error("PORT for http server is not specified!");
+        throw new Error("PORT for http server is not specified or invalid!");
 
     try {
         await mongoose.connect(MONGO_URI);
-        console.log("Database connected successfully!");
+        console.log("Connected to DB!");
 
         app.listen(HTTP_PORT, () => {
-            console.log(`HTTP server is running at port ${HTTP_PORT}!`);
+            console.log(`Running HTTP server at port ${HTTP_PORT}!`);
         });
     } catch(err) {
         console.error(err);
